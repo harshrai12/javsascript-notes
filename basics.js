2a6fb9aa52662fc816acab9579021d3c69ed7fdf
Harsh Rai <harshpayal3252@gmail.com>
2023-09-13 13:18:45 +0530

    javascript-datatypes

---
+++ basics.js
@@ -0,0 +1,65 @@
+var a= 10; //numbers
+console.log(a); //its an number
+console.log(typeof(a));
+var a="name" //strings
+console.log(a);//its a string
+console.log(typeof(a)); //its a string
+var a= true //boolean 
+console.log(a);  //its a boolean
+console.log(typeof(a));
+var a= [1,2,3] //arraay
+console.log(a); //its an array
+console.log(typeof(a)); //its an object
+var a ={         //object
+    day:"tuesday",
+    time:"4pm",
+}
+console.log(a);     //its an object
+console.log(typeof(a)); //its an object
+
+var a= null;   //null
+console.log(a); //its an object
+console.log(typeof(a));
+var a= undefined; //undefined
+console.log(a); //it undefined
+console.log(typeof(a));
+
+//
+
+const f =myfun =()=>{
+     console.log("hello");
+}
+
+console.log(typeof(f));
+
+const companies = [
+    {
+      name: "Company A",
+      industry: "Technology",
+      headquarters: "San Francisco, CA",
+      foundedYear: 2005
+    },
+    {
+      name: "Company B",
+      industry: "Retail",
+      headquarters: "New York, NY",
+      foundedYear: 1990
+    },
+    {
+      name: "Company C",
+      industry: "Healthcare",
+      headquarters: "Boston, MA",
+      foundedYear: 1975
+    }
+  ];
+  
+  // You can add more company objects to this array as needed.
+  
+  function myfunc() {
+    age=21;
+    console.log(name);
+    console.log(age);
+    var name = 'BBBootstrap';
+    let age;
+  }
+  myfunc();
